63dff8a6fcbcfb708828499422cdbfc88f1fcd1e
Daniel Jansen <44923461+xoladone@users.noreply.github.com>
2022-10-31 10:10:50 +0100

    Create motion_detection.py

---
+++ motion_detection.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+import os
+
+SENSOR_PIN = 22
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SENSOR_PIN, GPIO.IN)
+
+def display_callback(channel):
+    # print message for debugging
+    # print('Motion detected.')
+    os.system("xset -display :0.0 dpms force on")
+
+try:
+    GPIO.add_event_detect(SENSOR_PIN , GPIO.RISING, callback=display_callback)
+    while True:
+        time.sleep(100)
+except KeyboardInterrupt:
+    print "Finis."
+GPIO.cleanup()
